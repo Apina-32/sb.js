898d58248f3559ee92f98f4f567c5bc3c1aa9f9b
Samuli Ruuskanen <samuli.ruuskanen@tuni.fi>
2022-10-05 06:20:04 +0300

    Move rest of the setting to MonkeyConfig

--- build/header.user.js
+++ build/header.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         sb.js userscript
 // @description  SponsorBlock userscript
-// @namespace    mchang.name
+// @author       Apina-32
+// @namespace    https://a32.fi
 // @updateURL    https://github.com/Apina-32/sb.js/raw/main/docs/sb.user.js
+// @downloadURL  https://github.com/Apina-32/sb.js/raw/main/docs/sb.user.js
 // @homepage     https://github.com/Apina-32/sb.js
 // @icon         https://mchangrh.github.io/sb.js/icon.png
-// @version      1.2.3
+// @version      1.2.4
 // @license      LGPL-3.0-or-later
 // @match        https://www.youtube.com/*
 // @connect      sponsor.ajay.app
--- docs/sb-loader.user.js
+++ docs/sb-loader.user.js
@@ -16,23 +16,18 @@
 /* LICENCED UNDER LGPL-3.0-or-later */
 /* START OF SETTINGS */
 // Warning: editing setting here will disable auto updates
+// To edit settings, use the settings menu by clicking the tampermonkey icon on the top right of your browser
 
 // https://wiki.sponsor.ajay.app/w/Types
-let actionTypes = [
-  "skip",
-  "mute",
-  "full",
-  "poi"
-]
+
 let skipThreshold = [0.2, 1] // skip from between time-[0] and time+[1]
 let serverEndpoint = "https://sponsor.ajay.app"
 let skipTracking = true
 let highlightKey = "Enter"
 // https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values
 
-// DO NOT EDIT BELOW THIS LINE
 
-const settings = [
+const defaultCategories = [
     "sponsor",
     "selfpromo",
     "interaction",
@@ -44,10 +39,17 @@ const settings = [
     "poi_highlight"
 ]
 
-const params = () => {
+let defaultActionTypes = [
+    "skip",
+    "mute",
+    "full",
+    "poi"
+]
+
+const createCheckboxes = (list) => {
     const out = {}
-    settings.forEach(setting => {
-        out[setting] = {
+    list.forEach(item => {
+        out[item] = {
             type: 'checkbox',
             default: true
         }
@@ -55,16 +57,54 @@ const params = () => {
     return out;
 }
 
-
-const cfg = new MonkeyConfig({
-  title: 'Settings',
+const categoriesConfig = new MonkeyConfig({
+  title: 'Adjust SponsorBlock categories',
   menuCommand: true,
-  params: params()
+  params: createCheckboxes(defaultCategories)
+});
+
+const actionsConfig = new MonkeyConfig({
+    title: 'Adjust SponsorBlock actions',
+    menuCommand: true,
+    params: createCheckboxes(defaultActionTypes)
+});
+
+const miscConfig = new MonkeyConfig({
+    title: 'Miscellaneous settings',
+    menuCommand: true,
+    params: {
+        skipThreshold: {
+            type: 'text',
+            default: skipThreshold.join(", ")
+        },
+        serverEndpoint: {
+            type: 'text',
+            default: serverEndpoint
+        },
+        skipTracking: {
+            type: 'checkbox',
+            default: skipTracking
+        },
+        highlightKey: {
+            type: 'text',
+            default: highlightKey
+        }
+    }
 });
 
 const categories = [];
-settings.forEach(setting => {
-    if(cfg.get(setting)) categories.push(setting);
+defaultCategories.forEach(setting => {
+    if(categoriesConfig.get(setting)) categories.push(setting);
 });
+
+const actionTypes = [];
+defaultActionTypes.forEach(setting => {
+    if(actionsConfig.get(setting)) actionTypes.push(setting);
+});
+
+skipThreshold = miscConfig.get("skipThreshold").split(",").map(x => parseFloat(x));
+serverEndpoint = miscConfig.get("serverEndpoint");
+skipTracking = miscConfig.get("skipTracking");
+highlightKey = miscConfig.get("highlightKey");
 
 /* END OF SETTINGS */
